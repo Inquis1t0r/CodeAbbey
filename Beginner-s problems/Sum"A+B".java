8f142a334b9ee3ba2ca3d7e8e48317e0a01ddd01
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-06-27 10:41:54 +0200

    Create Sum"A+B".java

---
+++ Beginner-s problems/Sum"A+B".java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int first, second;
+        first = myScanner.nextInt();
+        second = myScanner.nextInt();
+        System.out.println(first + second);
+    }
+
+}
