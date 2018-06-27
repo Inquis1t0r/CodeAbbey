e27b9955d53152418bf77d889220aaff81a68f20
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-06-27 10:42:21 +0200

    Update Sum"A+B".java

--- Beginner-s problems/Sum"A+B".java
+++ Beginner-s problems/Sum"A+B".java
@@ -1,13 +1,18 @@
-import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        int first, second;
-        first = myScanner.nextInt();
-        second = myScanner.nextInt();
-        System.out.println(first + second);
+    /* package whatever; // don't place package name! */
+     
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+     
+    /* Name of the class has to be "Main" only if the class is public. */
+    class Ideone
+    {
+    	public static void main (String[] args) throws java.lang.Exception
+    	{
+    		        Scanner myScanner = new Scanner(System.in);
+            int first, second;
+            first = myScanner.nextInt();
+            second = myScanner.nextInt();
+            System.out.println(first + second);
+    	}
     }
-
-}
