ccd42cfeff00bf8bc5b47037444c88f48c185591
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2019-01-03 10:51:01 +0100

    Create Sum"A+B".kt

---
+++ Beginner-s problems/Sum"A+B".kt
@@ -0,0 +1,12 @@
+package playground
+
+import java.util.Scanner
+
+fun main(args : Array<String>) {
+
+
+    val input = Scanner(System.`in`)
+    val first = input.nextInt()
+    val second = input.nextInt()
+    println(first + second)
+}
