a6ff265b62bde4d08ad1ff586ffcde7624295817
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-10-28 12:55:31 +0100

    Update MinimumofTwo.js

--- Beginner-s problems/MinimumofTwo.js
+++ Beginner-s problems/MinimumofTwo.js
@@ -1,11 +1,10 @@
 const input = prompt();
 
-const lines = input.split('\n');
+const lines = input.split(' ');
 
 const numOfLines = parseInt(lines[0], 10);
 console.log(`Number of lines to read: ${numOfLines}`);
-
+console.log(lines);
 for (let i = 1; i <= numOfLines; i++) {
     console.log(`Pair ${i}: ${lines[i]}`);
 }
-
